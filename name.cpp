23ed917ed5df618d17e0c8bcb93940486b2ada25
Kumori <tauen8@gmail.com>
2024-02-01 17:36:07 -0600

    initial commit

---
+++ name.cpp
@@ -0,0 +1,15 @@
+
+// Taylor Golden
+//Status: Complete
+// This program will write the name, address and telephone
+// number of the programmer.
+#include <iostream>
+using namespace std;
+int main()
+{
+// Fill in this space to write your first and last name
+// Fill in this space to write your address (on new line)
+// Fill in this space to write you city, state and zip (on new line)
+// Fill in this space to write your telephone number (on new line)
+return 0;
+}
