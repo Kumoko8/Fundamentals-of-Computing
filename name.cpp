9944a49e94899ef86da869d1b823323d5bbf52a5
Kumori <tauen8@gmail.com>
2024-02-02 13:18:09 -0600

    using cout to print information

--- name.cpp
+++ name.cpp
@@ -8,8 +8,12 @@ using namespace std;
 int main()
 {
 // Fill in this space to write your first and last name
+    cout << "Taylor Golden" <<endl;
 // Fill in this space to write your address (on new line)
+    cout << "225 North Parkway" <<endl;
 // Fill in this space to write you city, state and zip (on new line)
+    cout << "Austin, TX 78665" <<endl;
 // Fill in this space to write your telephone number (on new line)
+    cout << "5122443525" <<endl;
 return 0;
 }
