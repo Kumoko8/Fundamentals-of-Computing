d144664cd7f7b8b4543a87d6f4e2e932a964f460
Kumori <tauen8@gmail.com>
2025-06-02 21:50:17 -0500

    completed area of shapes menu program still debugging

--- addNumbers.cpp
+++ addNumbers.cpp
@@ -2,26 +2,28 @@
 
 using namespace std;
 
-int main() {
-  //Create a number accumulator that adds any amount of numbers you choose
-  //I set the number and total to double to be able to handle large decimals as well
+int main()
+{
+  // Create a number accumulator that adds any amount of numbers you choose
+  // I set the number and total to double to be able to handle large decimals as well
   double number;
   int counterMax;
   double total = 0;
-  
+
   cout << "Enter how many numbers you wish to add together " << endl;
   cin >> counterMax;
-  
-  for (int counter = 1; counter <= counterMax; counter++){
-      cout << "Enter number ";
-      cin >> number;
-      total = total + number;
+
+  for (int counter = 1; counter <= counterMax; counter++)
+  {
+    cout << "Enter number ";
+    cin >> number;
+    total = total + number;
   }
-cout << "The total is " << total;
-    return 0;
+  cout << "The total is " << total;
+  return 0;
 }
 
-//I used programiz to play around with and make a program that adds any amount of numbers
-//My first mistake was forgetting the semicolons, very important!
-//then I had to edit my pseudocode to have the correct syntax for for loops (initialization; condition; increment)
-// I learned that if you don't include the increment for counter, then counter will never equal the counterMax and thus will forever ask for another number without exiting the loop and displaying the total
+// I used programiz to play around with and make a program that adds any amount of numbers
+// My first mistake was forgetting the semicolons, very important!
+// then I had to edit my pseudocode to have the correct syntax for for loops (initialization; condition; increment)
+//  I learned that if you don't include the increment for counter, then counter will never equal the counterMax and thus will forever ask for another number without exiting the loop and displaying the total
---
+++ areaMenu.cpp
@@ -0,0 +1,69 @@
+// A menu driven program with options 1-4 to calculate areas of three different shapes and an option to end program
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // Constant for calculating area
+    const double PI = 3.1415;
+
+    // necessary variables for user input
+    int user_choice;
+    double radius;
+    double circ_area;
+    double rect_length;
+    double rect_width;
+    double rect_area;
+    double tri_base;
+    double tri_height;
+    double tri_area;
+
+    // Menu Display and user input choice
+
+    cout << "   Calculate Area Menu    ";
+    cout << " 1. Area of a Circle ";
+    cout << " 2. Area of a Rectangle ";
+    cout << " 3. Area of a Triangle ";
+    cout << " 4. Quit ";
+
+    cout << " Select your desired shape ";
+    cin >> user_choice;
+
+    // Format the output of the area
+    cout << fixed << showpoint << setprecision(2);
+
+    // Configure calculations for each option
+
+    if (user_choice == 1)
+    {
+        cout << " Enter the radius of the circle ";
+        cin >> radius;
+        circ_area = PI * radius * radius;
+        cout << " The area of the circle is " << circ_area << endl;
+    }
+    else if (user_choice == 2)
+    {
+        cout << " Enter the length of the rectangle";
+        cin >> rect_length;
+        cout << " Enter the width of the rectangle";
+        cin >> rect_width;
+        rect_area = rect_length * rect_width;
+        cout << "  The area of the rectangle is " << rect_area << endl;
+    }
+    else if (user_choice == 3)
+    {
+        cout << " Enter the base of the triangle";
+        cin >> tri_base;
+        cout << " Enter the height of the triangle";
+        cin >> tri_height;
+        tri_area = tri_base * tri_height / 2;
+        cout << "  The area of the triangle is " << tri_area << endl;
+    }
+    else if (user_choice != 4)
+    {
+        cout << " Invalid option. Please choose an option 1 -4. Restart program";
+    }
+    return 0;
+}
