c2a80b730b9596a812b38cd7906273cffeb764b9
Kumori <tauen8@gmail.com>
2025-06-06 22:32:22 -0500

    started racer program

---
+++ racerTimes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    //initiate variables
+
+
+    string racer1;
+    string racer2;
+    string racer3;
+    double racer1time;
+    double racer2time;
+    double racer3time;
+
+    //Take in the runner and time data
+    cout << "Welcome to the Runner Program";
+    cout << "Enter the names of the three runners";
+    cin >> racer1;
+}
