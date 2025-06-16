27e12eaf07cdb284e2ae917ade59ccb54c7c3ab0
Kumori <tauen8@gmail.com>
2025-06-16 15:58:39 -0500

    completed basic functionality for calculating order total on spool orders

--- spoolOrders.cpp
+++ spoolOrders.cpp
@@ -8,7 +8,6 @@ int main(){
     double spoolStock;
     double extraShipping;
     int readytoShip;
-    double backorderSpools;
     double baseSpoolPrice;
     double totalShipping;
     double orderTotal;
@@ -22,19 +21,49 @@ int main(){
     cout << "Enter the number of spools ordered ";
     cin >> spoolNumber;
 
+    while(spoolNumber < 1){
+        cout << "Enter a number greater than 1 for spools ordered";
+        cin >> spoolNumber;
+    }
+
     cout << "Enter the number of spools in stock ";
     cin >> spoolStock;
+
+    while(spoolStock < 0){
+        cout << "Enter a positive number for spools in stock please";
+        cin >> spoolStock;
+    }
 
     cout << "Enter any additional shipping and handling costs (above the default $10) ";
     cin >> extraShipping;
 
+    while(extraShipping < 0){
+        cout << "Please enter a positive amount for extra shipping or 0 for none.";
+        cin >> extraShipping;
+
+    }
+
     cout << "The number of spools ordered is: " << spoolNumber  << endl;
     cout << "There are " << spoolStock - spoolNumber <<  " spools left in stock ";
 
+    if(spoolNumber > spoolStock){
+        cout << "There are " << spoolNumber - spoolStock << "spools on backorder ";
+    }
+    else {
+        cout << "There are no spools on backorder right now. ";
+    }
 
+    baseSpoolPrice = spoolNumber * spoolPrice;
+    cout << "The base price of spools ready to ship is " << baseSpoolPrice;
 
+    totalShipping = shipFee + extraShipping;
+    cout << "The total amount of shipping and handling is " << totalShipping;
 
+    orderTotal = baseSpoolPrice + totalShipping;
+    cout << "The order total is " << orderTotal;
 
 
 }
+
+
 
