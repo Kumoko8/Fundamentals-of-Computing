b4feda2a7e7ed3a802e964b8903c7c177b83b0aa
Kumori <tauen8@gmail.com>
2025-06-28 15:29:17 -0500

    edited format and used loop for input

--- chipsAndSalsaSales.cpp
+++ chipsAndSalsaSales.cpp
@@ -1,35 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int mildJars;
-int mediumJars;
-int sweetJars;
-int hotJars;
-int zestyJars;
-
-//array for salsa names
-
 const int SIZE = 5;
 int salsaAmounts[SIZE];
 
 int main()
 {
-string salsaNames[5] = {"Mild", "Medium", "Sweet", "Hot", "Zesty"};
-for (int i = 0; i < SIZE; i++)
-{
-     cout << "Enter number of" << salsaNames[i] << "jars sold";
-    cin >> salsaAmounts[i];
-}
-
-   
-    cout << "The sales for each are: ";
-    for (int i : salsaAmounts)
+    string salsaNames[5] = {"Mild", "Medium", "Sweet", "Hot", "Zesty"};
+    for (int i = 0; i < SIZE; i++)
     {
-       cout << i << endl;
+        cout << "Enter number of " << salsaNames[i] << " jars sold ";
+        cin >> salsaAmounts[i];
     }
-    
 
-
+    cout << "The sales for each are: " << endl;
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << salsaAmounts[i] << " " << salsaNames[i] << " jars" << endl;
+    }
 
     return 0;
 }
