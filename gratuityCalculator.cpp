c4481ad65627370e2259316b25414ed01074b30c
Kumori <tauen8@gmail.com>
2025-06-29 14:39:17 -0500

    added sentinel controlled loop for gratuity calc

--- gratuityCalculator.cpp
+++ gratuityCalculator.cpp
@@ -1,30 +1,27 @@
 // How to  a class
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 class Tips
 {
 
 private:
-    double taxRate = 0.085;
-   
+    double taxRate;
 
 public:
     // tax rate constructor
 
-    Tips(double rate)
+    Tips(double rate = 0.085)
     {
         taxRate = rate;
     }
 
     double computeTip(double totalBill, double tipRate)
     {
-        double mealCost = totalBill - (totalBill * taxRate);
+        double mealCost = totalBill / (1 + taxRate);
         return mealCost * tipRate;
-        
-
-        
     }
 };
 
@@ -35,19 +32,29 @@ int main()
     double totalTip;
     double taxRate;
 
+    cout << fixed << setprecision(2);
 
-    cout << "Enter the tax rate as a decimal (ex: .05)";
+    cout << "Enter the tax rate as a decimal (ex: .05) or enter -99 to exit ";
     cin >> taxRate;
-
+while(taxRate > 0) {
+    
     Tips meal1(taxRate);
-
-    cout << "Enter the total bill amount(tax included)";
+    
+    cout << "Enter the total bill amount(tax included) ";
     cin >> totalBill;
-
-    cout << "Enter the tip rate as a decimal (ex: .25)";
+    
+    cout << "Enter the tip rate as a decimal (ex: .25) ";
     cin >> tipRate;
+    
+    totalTip = meal1.computeTip(totalBill, tipRate);
+    
+    
+    cout << "The tip amount is " << totalTip << endl;
+    cout << "The total bill was " << totalBill << endl;
+    cout << "The cost of your meal was " << (totalTip / tipRate) << endl;
 
-    meal1.computeTip(totalBill, tipRate);
-
+    break;
+}
+cout << "See you next time!";
     return 0;
 }
