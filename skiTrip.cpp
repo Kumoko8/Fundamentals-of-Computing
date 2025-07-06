67fcabeba5c52d638155042dc809f31f31b15c1f
Kumori <tauen8@gmail.com>
2025-07-06 13:55:11 -0500

    fixing month input validation loop

--- skiTrip.cpp
+++ skiTrip.cpp
@@ -3,67 +3,84 @@
 #include <iomanip>
 using namespace std;
 
-class SkiTrip {
+class SkiTrip
+{
 
 private:
-int date;
-int inchesSnow;
-// We want the user to enter a name of the month (July for ex) and a start date in that month (like 14) and then a seven day period end date(which I might just make it startDate + 6) and be able to then enter the day and number of inches for each of the seven days, this means:
-// that the name of the month they set is just for display *
-//the start and end dates shouldn't really affect the objects because the dates should all be real dates, I mean I suppose I could make an array of days of the month so that they choose from it or something so you can't have like Sept 31 but that seems like a lot
-//
+    int date;
+    int inchesSnow;
+    // We want the user to enter a name of the month (July for ex) and a start date in that month (like 14) and then a seven day period end date(which I might just make it startDate + 6) and be able to then enter the day and number of inches for each of the seven days, this means:
+    // that the name of the month they set is just for display *
+    // the start and end dates shouldn't really affect the objects because the dates should all be real dates, I mean I suppose I could make an array of days of the month so that they choose from it or something so you can't have like Sept 31 but that seems like a lot
+    //
 
 public:
-SkiTrip(int d, int i) {
-    date = d;
-    inchesSnow = i;
-}
+    SkiTrip(int d, int i)
+    {
+        date = d;
+        inchesSnow = i;
+    }
 
-void setSnowfall() {
+    void setSnowfall()
+    {
 
-    return ;
-}
-
-
+        return;
+    }
 };
 
-int main() {
-    int SIZE = 12;
+int main()
+{
+    const int SIZE = 12;
     string months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-    int dates[] = {'31', '28', '31', '30', '31', '30', '31', '31', '30', '31','30', '31'};
+    int dates[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int endDate;
     int startDate;
 
     string snowMonth;
     int monthIndex = -1;
 
-    cout << "Enter the name of the month for the snow fall period";
-    cin >> snowMonth;
-
-    for ( int i = 0; i < SIZE; i++)
-    {
-        if (snowMonth == months[i]){
-            monthIndex = i;
-        // this now stores the index from the months array in monthIndex for the number of the month (ex: July (7)) 
-        // dates[monthIndex] is the number of days in that month
-            break;
-        }
-    }
-    
     while (monthIndex == -1)
     {
-        cout << "Invalid month!";
+
+        cout << "Enter the name of the month for the snow fall period ";
         cin >> snowMonth;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            if (snowMonth == months[i])
+            {
+                monthIndex = i + 1;
+                // this now stores the index from the months array in monthIndex for the number of the month (ex: July (7))
+                // dates[monthIndex] is the number of days in that month
+                break;
+            }
+        }
+        if (monthIndex == -1)
+        {
+            cout << "Invalid month! (Check your spelling, no abbreviations)";
+        }
     }
-    cout << "Enter the start date of the week";
+
+    cout << "Enter the start date of the week ";
     cin >> startDate;
 
-    while (startDate <= 0 || startDate > dates[monthIndex]){
-        cout << "Invalid date!";
+    while (startDate <= 0 || startDate > dates[monthIndex])
+    {
+        cout << "Invalid date! Enter start date ";
         cin >> startDate;
     }
 
+    cout << "Enter the end date of the week of snowfall ";
+    cin >> endDate;
 
-    cout << "Snow Report" << snowMonth << endl;
+    while (endDate <= 0)
+    {
+        cout << "Invalid end date! Enter valid end date. ";
+        cin >> endDate;
+    }
+
+    cout << "Snow Report " << snowMonth << " " << startDate << " - " << endDate << endl;
+    cout << dates[monthIndex];
 
     return 0;
 }
