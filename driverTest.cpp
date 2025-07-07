778b7880290af01f6ad9d0bc515aa21f1c175d3f
Kumori <tauen8@gmail.com>
2025-07-06 23:43:27 -0500

    fixed validation

--- driverTest.cpp
+++ driverTest.cpp
@@ -67,14 +67,37 @@ test.setKey(answerKey);
 string responses;
 string moreGrading;
 do {
+    while(true){
 
-    cout << " This is a test grader program. Enter each of your responses to be graded (a, b, c , or d) without any spaces.";
-    cin >> responses;
+        cout << " This is a test grader program. Enter each of your responses to be graded (a, b, c , or d) without any spaces.";
+        cin >> responses;
+
+        if(responses.length() != 20){
+            cout << "Please enter 20 answers with no spaces ";
+            continue;
+        }
+        
+        
+        bool allValid = true;
+        for (size_t i = 0; i < 20; i++)
+        {
+           if(!isValidResponse(responses[i])) {
+            cout << "Invalid response make sure you have 20 responses, no spaces and only a,b,c, or d";
+            allValid = false;
+           }
+        }
+        if (allValid) {
+            break;
+        }
+        cout << "Please submit all 20 in one line with valid letters. ";
+    }
+    
+   
+
     char userAnswers[20];
     for(size_t i = 0; i < 20; i++){
         userAnswers[i] = responses[i];
-    }
-
+    }   
     test.gradeTest(responses);
     
     cout << "Would you like to grade another test? (Y or N): ";
